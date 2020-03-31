abf8231d7415d1cea32378479da747f685b058d6
Tomasz DUDEK <isn.dudek.tomasz@gmail.com>
2020-03-31 15:36:42 +0200

    Ajout class, getters et setters

--- src/Comment.java
+++ src/Comment.java
@@ -1,2 +1,90 @@
+import java.util.Date;
+
 public class Comment {
+    private Date date; //date of the comment
+    private int idCommentaire; // is the (unique) identiﬁer of the comment (integer)
+    private int idUser; // is the (unique) identiﬁer of the user (integer)
+    private String comment; // is the content of the comment (string)
+    private String user; // is the name of the user (string)
+    private int pidCommentaire; // is the identiﬁer of the comment this comment comments (-1 if this comment comments a message)
+    private int pidMessage; // is the identiﬁer of the message this comment comments (-1 if this comment comments a comment)
+    private int score;
+
+    public Comment (Date p_date, int p_idC, int p_idU, String p_comment, String p_user, int p_pidC, int p_pidM){
+        date = p_date;
+        idCommentaire = p_idC;
+        idUser = p_idU;
+        comment = p_comment;
+        user = p_user;
+        pidCommentaire = p_pidC;
+        pidMessage = p_pidM;
+        score = 20;
+    }
+
+    /* ********** Setters *********** */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setIdCommentaire(int idCommentaire) {
+        this.idCommentaire = idCommentaire;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPidCommentaire(int pidCommentaire) {
+        this.pidCommentaire = pidCommentaire;
+    }
+
+    public void setPidMessage(int pidMessage) {
+        this.pidMessage = pidMessage;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /* ********** Getters *********** */
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getIdCommentaire() {
+        return idCommentaire;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getPidCommentaire() {
+        return pidCommentaire;
+    }
+
+    public int getPidMessage() {
+        return pidMessage;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
--- src/Message.java
+++ src/Message.java
@@ -1,2 +1,69 @@
+import java.util.Date;
+
 public class Message {
+    private Date date; //date of the message
+    private int idMessage; //is the (unique) identiﬁer of the message (integer)
+    private int idUser; //is the (unique) identiﬁer of the user (integer)
+    private String message; //is the content of the message (string)
+    private String user; //is the name of the user (string)
+    private int score;
+
+    public Message(Date pdate, int pidM, int pidU, String pmessage, String puser){
+        date = pdate;
+        idMessage = pidM;
+        idUser = pidU;
+        message = pmessage;
+        user = puser;
+        score = 20;
+    }
+
+    /* ********** Setters *********** */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setIdMessage(int idMessage) {
+        this.idMessage = idMessage;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /* ********** Getters *********** */
+    public Date getDate() {
+        return date;
+    }
+
+    public int getIdMessage() {
+        return idMessage;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
