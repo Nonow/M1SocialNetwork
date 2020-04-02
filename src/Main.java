1d95d9c325a88d811baf082b47c2f36a1107a4ad
Tomasz DUDEK <isn.dudek.tomasz@gmail.com>
2020-04-02 17:00:51 +0200

    ajout systeme sauvegarde message lu

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
-
+    public static void main(String[] args) {
+        ReadFile test = new ReadFile();
+        test.init();
+        System.out.println(test);
+    }
 }
 
--- src/ReadFile.java
+++ src/ReadFile.java
@@ -2,23 +2,58 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
+import java.util.Date;
+import java.util.ArrayList;
 
 import static java.lang.Thread.sleep;
 
 public class ReadFile {
+    protected ArrayList<Message> listMessage;
+    protected ArrayList<Comment> listComment;
 
-    public static void main(String[] args) {
+
+    public ReadFile (){
+        listMessage = new ArrayList<>();
+        listComment = new ArrayList<>();
+    }
+
+    public void init() {
         Random random = new Random();
+
         try (FileReader reader = new FileReader("src/reseauSocial.txt");
              BufferedReader br = new BufferedReader(reader)) {
 
             // read line by line
             String line;
             while ((line = br.readLine()) != null) {
+                Date date = new Date();
+                //on attend 1 à 3 seconde
                 sleep(3000/(random.nextInt(3)+1));
-                System.out.println(line);
+                System.out.println( date + " Nouvelle ligne lu :" + line);
+                // on analyse la ligne lu
+                String[] chaine = line.split("\\|");
+                //System.out.println(chaine.length);
+                //on verifie selon la longeur s'il s'agit d'un ajout de message ou commentaires
+                switch (chaine.length){
+                    case 4:
+                        //System.out.println("case 4");
+                        listMessage.add(new Message(date,Integer.parseInt(chaine[0]), Integer.parseInt(chaine[1]), chaine[2], chaine[3] ));
+                        break;
+                    case 5:
+                        //System.out.println("case 5");
+                        listComment.add(new Comment(date,Integer.parseInt(chaine[0]), Integer.parseInt(chaine[1]), chaine[2], chaine[3], Integer.parseInt(chaine[4]), -1 ));
+                        break;
+                    case 6:
+                        //System.out.println("case 6");
+                        listComment.add(new Comment(date,Integer.parseInt(chaine[0]), Integer.parseInt(chaine[1]), chaine[2], chaine[3], -1,  Integer.parseInt(chaine[5]) ));
+                        break;
+                    default:
+                        System.out.println("erreur lecture ligne du fichier");
+                }
+
             }
 
+            br.close();
         } catch (IOException | InterruptedException e) {
             System.err.format("IOException: %s%n", e);
         }
