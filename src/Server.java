88b5c73b5258eee12be5ed1af1bcc0a3022f86ed
Tomasz DUDEK <isn.dudek.tomasz@gmail.com>
2020-04-16 13:59:25 +0200

    Ajout thread server

--- src/Server.java
+++ src/Server.java
@@ -2,19 +2,11 @@ import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 import java.rmi.Naming;
-import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            //LocateRegistry.createRegistry(1199);
-
-            /*System.out.println("Mise en place du Security Manager ...");
-            if (System.getSecurityManager() == null) {
-                System.setSecurityManager(new RMISecurityManager());
-            }*/
             //instanciation de notre objet
             ReadFile data = new ReadFile();
             //Enregistrement de noms RMI
@@ -22,12 +14,30 @@ public class Server {
             System.out.println("Enregistrement de l'objet avec l'url : " + url);
             Naming.rebind(url, data);
 
-            //lancement de la lecture du fichier reseauSocial.txt
-            data.launch();
-            System.out.println("Serveur lancé");
-            data.Maj_score();
+            new Thread(){
+                @Override
+                public void run(){
+                    try {
+                        data.launch();
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
 
-            //System.out.println("Serveur lancé");
+            new Thread(){
+                @Override
+                public void run(){
+                    while(true){
+                        try {
+                            data.Maj_score();
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }.start();
+            System.out.println("Serveur lancé");
         } catch (RemoteException e) {
             e.printStackTrace();
         } catch (MalformedURLException e) {
